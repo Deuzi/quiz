7cfb217672ae80c37e263ed1f6367f311c32b4c4
chimdi ikechukwu <chimdiikechukwu00@gmail.com>
2025-04-04 01:01:32 +0100

    test

--- frontend-quiz-app/Quiz.js
+++ frontend-quiz-app/Quiz.js
@@ -1,7 +1,12 @@
 const themeToggle = document.querySelector('#slide');
 const htmlBody = document.querySelectorAll('html, body');
-
+const quizQuestions = document.querySelector('#quiz-question');
+const questionContent = document.querySelector('#questions');
+const progressBar = document.querySelector('.progress-bar');
+const progress = document.querySelector('#progress');
+const currentQuestion = document.querySelector('#current-question');
 //Dark mode switch
+
 function updateBackground(isDarkMode) {
   const mobile = window.matchMedia('(max-width: 599px)');
   const tablet = window.matchMedia(
@@ -66,3 +71,48 @@ window.addEventListener('resize', () => {
   const isDarkMode = themeToggle.checked;
   updateBackground(isDarkMode);
 });
+
+//Dark Mode Switch end
+
+const subjects = document.querySelectorAll('.subjects');
+
+subjects.forEach((subject) => {
+  subject.addEventListener('click', () => {
+    const quizMenu = document.querySelector('#quiz-menu');
+    quizMenu.style.display = 'none';
+    quizQuestions.style.display = 'block';
+  });
+});
+
+fetch('frontend-quiz-app/starter-code/data.json')
+  .then((Response) => {
+    if (!Response.ok) {
+      throw new Error('failed to fetch quiz');
+    }
+    return Response.json();
+  })
+  .then((data) => {
+    const subjectHtml = document.querySelector('#html');
+    subjectHtml.addEventListener('click', () => {
+      const quiz = data.quizzes[0];
+      const logo = document.createElement('img');
+      logo.classList.add('html-svg');
+      logo.src = quiz.icon;
+      const logoSection = document.getElementById('subject-name-and-logo');
+      const sectionName = document.querySelector('.section-name');
+      logoSection.appendChild(logo);
+      logoSection.insertBefore(logo, sectionName);
+      sectionName.textContent = `${quiz.title}`;
+
+      //
+      document.getElementById('questions').textContent =
+        quiz.questions[0].question;
+
+      const optionsLi = document.querySelectorAll('#question-list li');
+      quiz.questions[0].options.forEach((option, index) => {
+        const answerTag = optionsLi[index].querySelector('.answer-tag');
+        optionsLi[index].textContent = option;
+        optionsLi[index].prepend(answerTag);
+      });
+    });
+  });
